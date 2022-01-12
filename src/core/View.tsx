122ad3bd340812cf7f5e7e35e6cfac8211173dd2
Israel Batista <dev.israel.batista@gmail.com>
2022-01-12 10:48:39 -0300

    Using react for the frontend

---
+++ src/core/View.tsx
@@ -0,0 +1,110 @@
+import React, { useEffect, useRef } from "react";
+import { fromEvent, map, mergeWith, Subject } from "rxjs";
+import { Vec2D } from "../utils/math";
+import { drawTurtleUpdate } from "./drawingFunctions";
+import Environment from "./Environment";
+import { EnvironmentChanges, TurtleAction, UserInputData } from "./Streams";
+import Turtle from "./Turtle";
+
+const setupCanvas = (env: Environment, ctx: CanvasRenderingContext2D) => {
+  ctx.fillStyle = env.backgroundColor;
+  ctx.strokeStyle = env.penColor;
+  ctx.fillRect(0, 0, env.width, env.height);
+  ctx.moveTo(...env.transform(new Vec2D(0, 0)).cartesianComponents);
+};
+
+export function App() {
+  const canvasRef = useRef(null);
+  const environmentUpdates = new Subject<EnvironmentChanges>();
+  const turtleUpdates = new Subject<TurtleAction>();
+
+  const turtle = new Turtle(turtleUpdates);
+  const env = new Environment(environmentUpdates);
+
+  useEffect(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    const context = canvas.getContext("2d");
+    setupCanvas(env, context);
+
+    const userActionEvents = [
+      fromEvent(document.getElementById("Turtle.fd"), "click"),
+      fromEvent(document.getElementById("Turtle.bd"), "click"),
+      fromEvent(document.getElementById("Turtle.rt"), "click"),
+      fromEvent(document.getElementById("Turtle.lt"), "click"),
+    ] as const;
+
+    const transformActionEvent = (ev: Event) => {
+      const buttonElement = ev.target as HTMLButtonElement;
+      const inputElement = document.querySelector(
+        `[data-payload-target="${buttonElement.id}"]`
+      ) as HTMLInputElement;
+      const payload = parseInt(inputElement.value);
+
+      // Clears the input before returning
+      inputElement.value = "";
+
+      return {
+        command: (ev.target as HTMLButtonElement).id,
+        payload: payload,
+      } as UserInputData;
+    };
+
+    userActionEvents[0]
+      .pipe(mergeWith(...userActionEvents.slice(1)), map(transformActionEvent))
+      .subscribe((update) => turtle.handleUpdate(update));
+
+    // Turtle updates stream
+
+    turtleUpdates.subscribe((update) =>
+      drawTurtleUpdate(context, env, turtle, update)
+    );
+  });
+
+  return (
+    <div>
+      <canvas ref={canvasRef} width={600} height={400}></canvas>
+
+      <div>
+        <div>
+          <input
+            type="number"
+            name="Forward-data"
+            id="fd-data"
+            data-payload-target="Turtle.fd"
+          />
+          <button id="Turtle.fd">Forward</button>
+        </div>
+
+        <div>
+          <input
+            type="number"
+            name="Forward-data"
+            id="fd-data"
+            data-payload-target="Turtle.bd"
+          />
+          <button id="Turtle.bd">Backwards</button>
+        </div>
+
+        <div>
+          <input
+            type="number"
+            name="Forward-data"
+            id="fd-data"
+            data-payload-target="Turtle.lt"
+          />
+          <button id="Turtle.lt">Left</button>
+        </div>
+
+        <div>
+          <input
+            type="number"
+            name="Forward-data"
+            id="fd-data"
+            data-payload-target="Turtle.rt"
+          />
+          <button id="Turtle.rt">Right</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./core/View";
+
+console.log("Script loaded");
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// import Turtle from "./core/Turtle";
+// import Environment from "./core/Environment";
+
+// const handleCommand = (
+//   turtle: Turtle,
+//   command/: "fd" | "bd" | "lt" | "rt",
+//   value: number
+// ) => {
+//   if (isNaN(value)) {
+//     value = 0;
+//   }
+
+//   switch (command) {
+//     case "fd":
+//       turtle.forward(value);
+//       break;
+
+//     case "bd":
+//       turtle.backwards(value);
+//       break;
+
+//     case "lt":
+//       turtle.rotateLeft(value);
+//       break;
+
+//     case "rt":
+//       turtle.rotateRight(value);
+//       break;
+//   }
+// };
+
+// const setupEvents = (root: HTMLElement, turtle: Turtle) => {
+//   const buttons = root.querySelectorAll("button");
+
+//   for (const button of buttons) {
+//     let command = button.dataset.command as "fd" | "bd" | "lt" | "rt";
+//     let input = root.querySelector(`#${command}`) as HTMLInputElement;
+
+//     button.addEventListener("click", (ev) => {
+//       const value = input.valueAsNumber;
+//       console.log(turtle);
+//       // console.log(command);
+//       console.log(value);
+//       handleCommand(turtle, command, value);
+//       input.value = "";
+//     });
+//   }
+// };
+
+// const setupTurtle = (): Turtle => {
+//   const env = new Environment(document.querySelector("canvas"));
+//   const turtle = new Turtle(env);
+
+//   // Position at center
+//   turtle.penUp();
+//   turtle.moveTo(env.width / 2, env.height / 2);
+//   turtle.penDown();
+//   turtle.setColor("white");
+
+//   return turtle;
+// };
+
+// (function main() {
+//   const t = setupTurtle();
+//   setupEvents(document.querySelector(".controls"), t);
+// })();
