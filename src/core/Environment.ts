7c134f03c4fac673670729e3271431b4f88a2b17
Israel Batista <dev.israel.batista@gmail.com>
2022-01-14 16:00:11 -0300

    Documented environment class

--- src/core/Environment.ts
+++ src/core/Environment.ts
@@ -2,6 +2,10 @@
 import { Subject } from "rxjs";
 import { Vec2D } from "../utils/math";
 
+/**
+ * Object for keeping the information about the environment the turtle lives in.
+ * Currently mainly used for background color and matrix transformations.
+ */
 export default class Environment {
   private _output: Subject<EnvironmentChanges>;
   private _width: number;
@@ -25,23 +29,39 @@ export default class Environment {
     this._output = outputStream;
   }
 
+  /**
+   * Width of drawing canvas.
+   */
   get width() {
     return this._width;
   }
 
+  /**
+   * Height of drawing canvas
+   */
   get height() {
     return this._height;
   }
 
+  /**
+   * Background color of the canvas
+   */
   get backgroundColor() {
     return this._backgroundColor;
   }
 
+  /**
+   * Background color of the canvas
+   */
   set backgroundColor(color) {
     this._backgroundColor = color;
     this._output.next("EnvironmentChange.bgColor");
   }
 
+  /**
+   * The turtle pen color
+   * @deprecated will be moved to the turtle class
+   */
   get penColor() {
     return this._penColor;
   }
@@ -51,11 +71,20 @@ export default class Environment {
     this._output.next("EnvironmentChange.penColor");
   }
 
+  /**
+   * Transforms the turtle position (or any other vector) from the standard cartesian space
+   * to the representation on the canvas.
+   * @param position Turtle position in standard coordinates
+   * @returns Turtle position adjusted to the current canvas
+   */
   transform(position: Vec2D): Vec2D {
     return new Vec2D(position.x + this.width / 2, this.height / 2 - position.y);
   }
 }
 
+/**
+ * A type that represents possible changes to the environment state
+ */
 export type EnvironmentChanges =
   | "EnvironmentChange.bgColor"
   | "EnvironmentChange.penColor";
