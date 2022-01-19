952b1bfa5541cc07d3a7a030e54ae0a3f7ecbdc6
Israel Batista <dev.israel.batista@gmail.com>
2022-01-19 10:18:11 -0300

    Refactor API to be object-based

---
+++ src/core/API/InputAPI.ts
@@ -0,0 +1,57 @@
+/**
+ * Interfaces that represent the messages passed from the input layer to the data layer
+ * @module
+ */
+
+export type InputCommand = TurtleCommand;
+
+interface BaseCommand {
+  target: "Turtle" | "Environment";
+  type: string;
+  data?: unknown;
+}
+
+// ========== Turtle commands ==========
+
+const possibleTurtleCommands = ["fd", "bk", "lt", "rt", "pu", "pd"] as const;
+export type TurtleCommand =
+  | TurtleForward
+  | TurtleBackward
+  | TurtleLeft
+  | TurtleRight
+  | TurtlePenUp
+  | TurtlePenDown;
+
+interface BaseTurtleCommand extends BaseCommand {
+  target: "Turtle";
+  type: typeof possibleTurtleCommands[number];
+  data?: unknown;
+}
+
+export interface TurtleForward extends BaseTurtleCommand {
+  type: "fd";
+  data: number;
+}
+
+export interface TurtleBackward extends BaseTurtleCommand {
+  type: "bk";
+  data: number;
+}
+
+export interface TurtleLeft extends BaseTurtleCommand {
+  type: "lt";
+  data: number;
+}
+
+export interface TurtleRight extends BaseTurtleCommand {
+  type: "rt";
+  data: number;
+}
+
+export interface TurtlePenUp extends BaseTurtleCommand {
+  type: "pu";
+}
+
+export interface TurtlePenDown extends BaseTurtleCommand {
+  type: "pd";
+}
---
+++ src/core/API/OutputAPI.ts
@@ -0,0 +1,87 @@
+/**
+ * Interfaces that represents the messages sent from the data layer to the presentation layer
+ * @module
+ */
+
+export type ViewUpdate = TurtleUpdate | EnvironmentUpdate;
+
+interface BaseUpdate {
+  from: "Turtle" | "Environment";
+  type: string;
+  data?: unknown;
+}
+
+// Turtle Updates
+
+const possibleTurtleUpdates = [
+  "beginUpdate",
+  "position",
+  "direction",
+  "pen",
+  "visibility",
+] as const;
+
+export type TurtleUpdate =
+  | TurtleBeginUpdate
+  | TurtleUpdatePosition
+  | TurtleUpdateDirection
+  | TurtleUpdatePen
+  | TurtleUpdateVisibility;
+
+interface BaseTurtleUpdate extends BaseUpdate {
+  from: "Turtle";
+  type: typeof possibleTurtleUpdates[number];
+  data?: Record<string, unknown>;
+}
+
+export interface TurtleBeginUpdate extends BaseTurtleUpdate {
+  type: "beginUpdate";
+}
+
+export interface TurtleUpdatePosition extends BaseTurtleUpdate {
+  type: "position";
+  data: {
+    x: number;
+    y: number;
+  };
+}
+
+export interface TurtleUpdateDirection extends BaseTurtleUpdate {
+  type: "direction";
+  data: {
+    direction: number;
+  };
+}
+
+export interface TurtleUpdatePen extends BaseTurtleUpdate {
+  type: "pen";
+  data: {
+    color: string;
+    isPenDown: boolean;
+  };
+}
+
+export interface TurtleUpdateVisibility extends BaseTurtleUpdate {
+  type: "visibility";
+  data: {
+    isVisible: boolean;
+  };
+}
+
+// Environment updates
+
+const possibleEnvironmentUpdates = ["backgroundColor"] as const;
+type EnvironmentUpdate = BackgroundColorUpdate;
+
+interface BaseEnvironmentUpdate extends BaseUpdate {
+  from: "Environment";
+  type: typeof possibleEnvironmentUpdates[number];
+  data: Record<string, unknown>;
+}
+
+interface BackgroundColorUpdate extends BaseEnvironmentUpdate {
+  type: "backgroundColor";
+  data: {
+    color: string;
+  };
+}
